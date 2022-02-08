925a9f4f9e43838cf4187074e107e7bef05d182e
George Borsato <georginhoborsato@gmail.com>
2022-02-08 11:24:32 -0300

    feat: querys

--- main.go
+++ main.go
@@ -34,8 +34,56 @@ func main() {
 	//}
 
 	//Createwith associations - Método de inserção aninhada igual rails
-	result := db.Create(&models.User{Name: "George", LastName: "Borsato", Cpf: "064.111.381-17",
-		CreditCard: models.CreditCard{Number: "112-77"}})
+	//result := db.Create(&models.User{Name: "George", LastName: "Borsato", Cpf: "064.111.381-17",
+	//	CreditCard: models.CreditCard{Number: "112-77"}})
 
-	fmt.Println(result.RowsAffected)
+	//fmt.Println(result.RowsAffected)
+
+	//querys
+
+	//product := &models.Product{}
+	//result := db.First(product) // FIRST - LAST - TAKE - retorna um unico registro
+	//
+	//if result.Error != nil {
+	//	println(err)
+	//}
+	//fmt.Println(product)
+
+	//var product models.Product
+	////var products []models.Product
+	//
+	//resultOne := db.First(&product)
+	//result := map[string]interface{}{}
+	//db.Model(&models.User{}).First(&result)
+
+	//product := &models.Product{}
+	//result := db.First(product, 4)
+	//
+	//if result.Error != nil {
+	//	println(result.Error)
+	//}
+	//fmt.Println(product)
+
+	//products := []models.Product{}
+	//db.Find(&products) //all products
+	//
+	//for _, product := range products {
+	//	fmt.Println(product)
+	//}
+
+	//product := models.Product{}
+	//db.Where("name like ?", "%arroz%").Find(&product)
+	//fmt.Println(product.Name)
+
+	//product := models.Product{}
+	//name := "arroz"
+	//db.Where(&models.Product{Name: name}, "Price").First(&product) // query utilizando struct -> muito parecido com querys usando objetos do rails
+	//fmt.Println(product)
+
+	products := []models.Product{}
+	db.Find(&products, models.Product{Name: "linguica"}) //podemos passar condições nas querys first and find
+
+	for _, value := range products {
+		fmt.Println(value)
+	}
 }
